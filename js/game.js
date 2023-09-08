124ff0e34f10d03f41548fd3499ad9beeb6a1061
Gahaaa <103615847+Gahaaa@users.noreply.github.com>
2023-09-08 10:05:00 +0900

    script 수정

--- js/game.js
+++ js/game.js
@@ -22,16 +22,17 @@ fetch(url)
         var nameTags = xmlDoc.getElementsByTagName('STATION_NM');
         var numTags = xmlDoc.getElementsByTagName('LINE_NUM');
 
-        console.log(xmlDoc)
+        // console.log(nameTags.textContent);
         for (var i = 0; i < nameTags.length; i++) {
             subwayNM.push(nameTags[i].textContent);
             subwayLine.push(numTags[i].textContent);
 
-        }
 
-        // console.log(subwayNM);
-        // console.log(subwayLine);
-        setGame();
+        }
+        console.log(subwayNM);
+
+        randomQuiz();
+        chkAnswr();
 
 
     })
@@ -42,28 +43,26 @@ fetch(url)
 const quizBg = document.querySelector('.quiz_area >div:first-of-type');
 const quizTit = document.querySelector('.quiz_area .quiz > p span');
 const quizBtn = document.getElementById('quiz_submit');
+let randomLine = Math.floor(Math.random() * 4+1);
+let randomLineTxt = `${randomLine}호선`;
 
 function randomQuiz() {
     randomLine = Math.floor(Math.random() * 4+1);
     randomLineTxt = `${randomLine}호선`;
 
-    quizBg.classList.remove('line1','line2','line3');
+    quizBg.classList.remove('line1','line2','line3','line4');
     quizBg.classList.add(`line${randomLine}`);
 
     quizTit.innerText = randomLineTxt;
 
 }
 
-function setGame() {
-    randomQuiz();
-    quizBg.classList.add(`line${randomLine}`);
-    chkAnswr(randomLineTxt);
 
-}
-
-function chkAnswr(randomLineTxt){
+function chkAnswr(){
     quizBtn.addEventListener('submit', (e)=>{
         e.preventDefault();
+        // 푼문제 카운트
+        myQuiz+=1;
 
         let answr = document.getElementById('subwayName').value;
         let answrNum = subwayLine[subwayNM.indexOf(answr)];
@@ -76,7 +75,6 @@ function chkAnswr(randomLineTxt){
             // 문제 호선과 일치할 때
             if(answrNum === `0${randomLineTxt}`){
                 nextQuiz(answr);
-                alert('맞았습니다.');
                 clearAnswr();
 
                 // 중복 노선 처리
@@ -88,7 +86,6 @@ function chkAnswr(randomLineTxt){
                         console.log(`0${randomLineTxt}`);
 
                         nextQuiz(answr);
-                        alert('맞았습니다.');
                         clearAnswr();
                         return false;
                     }
@@ -99,6 +96,8 @@ function chkAnswr(randomLineTxt){
                 alert('틀렸습니다.');
                 clearAnswr();
                 console.log('배열에 존재하지만 답이 아님');
+                console.log(result.length);
+                console.log(answrNum + "," + randomLineTxt);
 
             }
 
@@ -110,30 +109,31 @@ function chkAnswr(randomLineTxt){
         }
 
 
+
     });
 
 
 }
 
-// 정답일 때 처리
+// 정답일 때 처리(중복체크/스코어)
 function nextQuiz(answr){
     if(myAnswr.includes(answr)){
         alert("중복입력");
         return false;
     }
+    alert('맞았습니다.');
     myAnswr.push(answr);
-    myScore=myQuiz*10;
+    myScore = myScore + 10;
 
 }
-// input값 초기화& 푼문제 카운트
+// input값 초기화
 function clearAnswr() {
-    if(myQuiz === 9){
+    if(myQuiz === 10){
         alert('퀴즈 끝!');
 
     }
     randomQuiz();
     document.getElementById('subwayName').value = null;
-    myQuiz+=1;
     console.log(myQuiz);
     console.log(myScore);
 }
